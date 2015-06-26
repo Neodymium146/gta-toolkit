967b9a5c12d2f1d97529717aa5f367219414c969
Neodymium <neodymium146@eclipso.ch>
2015-06-26 20:46:14 +0200

    Added waypoint record resources

---
+++ RageLib.GTA5/Resources/PC/WaypointRecords/WayPointRecord_GTA5_pc.cs
@@ -0,0 +1,102 @@
+/*
+    Copyright(c) 2015 Neodymium
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+using RageLib.Resources.Common;
+using System.Collections.Generic;
+
+namespace RageLib.Resources.GTA5.PC.WaypointRecords
+{
+    public class WayPointRecord_GTA5_pc : FileBase64_GTA5_pc
+    {
+        public override long Length
+        {
+            get { return 48; }
+        }
+
+        // structure data
+        public uint Unknown_10h; // 0x00000000
+        public uint Unknown_14h; // 0x00000000
+        public ulong EntriesPointer;
+        public uint EntriesCount;
+        public uint Unknown_24h; // 0x00000000
+        public uint Unknown_28h; // 0x00000000
+        public uint Unknown_2Ch; // 0x00000000
+
+        // reference data
+        public ResourceSimpleArray<WaypointRecordEntry_GTA5_pc> Entries;
+
+        /// <summary>
+        /// Reads the data-block from a stream.
+        /// </summary>
+        public override void Read(ResourceDataReader reader, params object[] parameters)
+        {
+            base.Read(reader, parameters);
+
+            // read structure data
+            this.Unknown_10h = reader.ReadUInt32();
+            this.Unknown_14h = reader.ReadUInt32();
+            this.EntriesPointer = reader.ReadUInt64();
+            this.EntriesCount = reader.ReadUInt32();
+            this.Unknown_24h = reader.ReadUInt32();
+            this.Unknown_28h = reader.ReadUInt32();
+            this.Unknown_2Ch = reader.ReadUInt32();
+
+            // read reference data
+            this.Entries = reader.ReadBlockAt<ResourceSimpleArray<WaypointRecordEntry_GTA5_pc>>(
+                this.EntriesPointer, // offset
+                this.EntriesCount
+            );
+        }
+
+        /// <summary>
+        /// Writes the data-block to a stream.
+        /// </summary>
+        public override void Write(ResourceDataWriter writer, params object[] parameters)
+        {
+            base.Write(writer, parameters);
+
+            // update structure data
+            this.EntriesPointer = (ulong)(this.Entries != null ? this.Entries.Position : 0);
+            this.EntriesCount = (uint)(this.Entries != null ? this.Entries.Count : 0);
+
+            // write structure data
+            writer.Write(this.Unknown_10h);
+            writer.Write(this.Unknown_14h);
+            writer.Write(this.EntriesPointer);
+            writer.Write(this.EntriesCount);
+            writer.Write(this.Unknown_24h);
+            writer.Write(this.Unknown_28h);
+            writer.Write(this.Unknown_2Ch);
+        }
+
+        /// <summary>
+        /// Returns a list of data blocks which are referenced by this block.
+        /// </summary>
+        public override IResourceBlock[] GetReferences()
+        {
+            var list = new List<IResourceBlock>(base.GetReferences());
+            if (Entries != null) list.Add(Entries);
+            return list.ToArray();
+        }
+
+    }
+}
---
+++ RageLib.GTA5/Resources/PC/WaypointRecords/WaypointRecordEntry_GTA5_pc.cs
@@ -0,0 +1,71 @@
+/*
+    Copyright(c) 2015 Neodymium
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+namespace RageLib.Resources.GTA5.PC.WaypointRecords
+{
+    public class WaypointRecordEntry_GTA5_pc : ResourceSystemBlock
+    {
+        public override long Length
+        {
+            get { return 20; }
+        }
+
+        // structure data
+        public float PositionX;
+        public float PositionY;
+        public float PositionZ;
+        public ushort Unknown_Ch;
+        public ushort Unknown_Eh;
+        public ushort Unknown_10h;
+        public ushort Unknown_12h;
+
+        /// <summary>
+        /// Reads the data-block from a stream.
+        /// </summary>
+        public override void Read(ResourceDataReader reader, params object[] parameters)
+        {
+            // read structure data
+            this.PositionX = reader.ReadSingle();
+            this.PositionY = reader.ReadSingle();
+            this.PositionZ = reader.ReadSingle();
+            this.Unknown_Ch = reader.ReadUInt16();
+            this.Unknown_Eh = reader.ReadUInt16();
+            this.Unknown_10h = reader.ReadUInt16();
+            this.Unknown_12h = reader.ReadUInt16();
+        }
+
+        /// <summary>
+        /// Writes the data-block to a stream.
+        /// </summary>
+        public override void Write(ResourceDataWriter writer, params object[] parameters)
+        {
+            // write structure data
+            writer.Write(this.PositionX);
+            writer.Write(this.PositionY);
+            writer.Write(this.PositionZ);
+            writer.Write(this.Unknown_Ch);
+            writer.Write(this.Unknown_Eh);
+            writer.Write(this.Unknown_10h);
+            writer.Write(this.Unknown_12h);
+        }
+    }
+}
