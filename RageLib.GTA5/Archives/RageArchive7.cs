af3609956e3dcb62262dabc11be4458ed9aaa7ba
Neodymium <neodymium146@outlook.com>
2015-09-14 21:10:16 +0200

    Added support for RPF entries with size bigger than 16MB

--- RageLib.GTA5/Archives/RageArchive7.cs
+++ RageLib.GTA5/Archives/RageArchive7.cs
@@ -167,6 +167,14 @@ namespace RageLib.GTA5.Archives
                         var e = new RageArchiveResourceFile7();
                         e.Read(entries_reader);
 
+                        // there are sometimes resources with length=0xffffff which actually
+                        // means length>=0xffffff
+                        if (e.FileSize == 0xFFFFFF)
+                        {
+                            reader.Position = 512 * e.FileOffset;
+                            var buf = reader.ReadBytes(16);
+                            e.FileSize = ((uint)buf[7] << 0) | ((uint)buf[14] << 8) | ((uint)buf[5] << 16) | ((uint)buf[2] << 24);
+                        }
 
                         names_reader.Position = e.NameOffset;
                         e.Name = names_reader.ReadString();
@@ -275,6 +283,32 @@ namespace RageLib.GTA5.Archives
                     if (xx is RageArchiveDirectory7)
                         stack.Push((RageArchiveDirectory7)xx);
             }
+
+
+            // there are sometimes resources with length>=0xffffff which actually
+            // means length=0xffffff
+            // -> we therefore just cut the file size
+            foreach (var entry in entries)
+                if (entry is RageArchiveResourceFile7)
+                {
+                    var resource = entry as RageArchiveResourceFile7;
+                    if (resource.FileSize > 0xFFFFFF)
+                    {
+                        var buf = new byte[16];
+                        buf[7] = (byte)((resource.FileSize >> 0) & 0xFF);
+                        buf[14] = (byte)((resource.FileSize >> 8) & 0xFF);
+                        buf[5] = (byte)((resource.FileSize >> 16) & 0xFF);
+                        buf[2] = (byte)((resource.FileSize >> 24) & 0xFF);
+
+                        if (writer.Length > 512 * resource.FileOffset)
+                        {
+                            writer.Position = 512 * resource.FileOffset;
+                            writer.Write(buf);
+                        }                     
+
+                        resource.FileSize = 0xFFFFFF;
+                    }                        
+                }
 
 
             // entries...
