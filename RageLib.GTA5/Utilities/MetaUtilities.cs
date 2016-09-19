4308772780a3ade597d0a6db00fa7571aea31cc7
Neodymium <neodymium146@outlook.com>
2016-09-19 10:16:41 +0200

    Added utilities for collecting names from xml files

--- RageLib.GTA5/Utilities/MetaUtilities.cs
+++ RageLib.GTA5/Utilities/MetaUtilities.cs
@@ -20,11 +20,13 @@
     THE SOFTWARE.
 */
 
+using HtmlAgilityPack;
 using RageLib.Cryptography;
 using RageLib.GTA5.Archives;
 using RageLib.GTA5.Cryptography;
 using RageLib.GTA5.PSO;
 using RageLib.GTA5.Resources.PC;
+using RageLib.Hash;
 using RageLib.Resources.GTA5;
 using RageLib.Resources.GTA5.PC.Meta;
 using System;
@@ -171,6 +173,120 @@ namespace RageLib.GTA5.Utilities
                 }
             });
             return hashes;
+        }
+        
+        public static HashSet<string> GetAllStringsFromAllXmls(string gameDirectoryName)
+        {
+            var xmlStrings = new HashSet<string>();
+
+            ArchiveUtilities.ForEachBinaryFile(gameDirectoryName, (fullFileName, file, encryption) =>
+            {
+                if (file.Name.EndsWith(".meta", StringComparison.OrdinalIgnoreCase) ||
+                 file.Name.EndsWith(".xml", StringComparison.OrdinalIgnoreCase))
+                {
+                    var fileStream = new MemoryStream();
+                    file.Export(fileStream);
+
+                    var buf = new byte[fileStream.Length];
+                    fileStream.Position = 0;
+                    fileStream.Read(buf, 0, buf.Length);
+
+                    if (file.IsEncrypted)
+                    {
+                        if (encryption == RageArchiveEncryption7.AES)
+                        {
+                            buf = AesEncryption.DecryptData(buf, GTA5Constants.PC_AES_KEY);
+                        }
+                        else
+                        {
+                            var qq = GTA5Hash.CalculateHash(file.Name);
+                            var gg = (qq + (uint)file.UncompressedSize + (101 - 40)) % 0x65;
+                            buf = GTA5Crypto.Decrypt(buf, GTA5Constants.PC_NG_KEYS[gg]);
+                        }
+                    }
+
+                    if (file.IsCompressed)
+                    {
+                        var def = new DeflateStream(new MemoryStream(buf), CompressionMode.Decompress);
+                        var bufnew = new byte[file.UncompressedSize];
+                        def.Read(bufnew, 0, (int)file.UncompressedSize);
+                        buf = bufnew;
+                    }
+
+                    var cleanedStream = new MemoryStream(buf);
+                    foreach (string xmlString in GetAllStringsFromXml(cleanedStream))
+                    {
+                        xmlStrings.Add(xmlString);
+                    }
+
+                    Console.WriteLine(file.Name);
+                }
+            });
+
+            return xmlStrings;
+        }
+
+        public static HashSet<string> GetAllStringsFromXml(string xmlFileName)
+        {
+            using (var xmlFileStream = new FileStream(xmlFileName, FileMode.Open))
+            {
+                return GetAllStringsFromXml(xmlFileStream);
+            }
+        }
+
+        public static HashSet<string> GetAllStringsFromXml(Stream xmlFileStream)
+        {
+            var xmlStrings = new HashSet<string>();
+
+            var document = new HtmlDocument();
+            document.OptionOutputOriginalCase = true;
+            document.OptionWriteEmptyNodes = true;
+            document.Load(xmlFileStream);
+
+            var stack = new Stack<HtmlNode>();
+            stack.Push(document.DocumentNode);
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                foreach (var descendantNode in node.Descendants())
+                {
+                    stack.Push(descendantNode);
+                }
+                if (node.NodeType == HtmlNodeType.Text)
+                {
+                    xmlStrings.Add(node.InnerText.Trim());
+
+                    // for flags...
+                    string[] splitted = node.InnerText.Split(new char[] {' ','\t','\n' },StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var x in splitted)
+                    {
+                        xmlStrings.Add(x.Trim());
+                    }
+                }
+                if (node.NodeType == HtmlNodeType.Element)
+                {
+                    xmlStrings.Add(node.OriginalName.Trim());
+                    foreach (var attribute in node.Attributes)
+                    {
+                        xmlStrings.Add(attribute.OriginalName.Trim());
+                        xmlStrings.Add(attribute.Value.Trim());
+                    }
+                }
+            }
+
+            return xmlStrings;
+        }
+
+        public static HashSet<string> GetAllStringsThatMatchAHash(IEnumerable<string> listOfStrings, ISet<int> listOfHashes)
+        {
+            var matchingStrings = new HashSet<string>();
+            foreach (var s in listOfStrings)
+            {
+                int hash = unchecked((int)Jenkins.Hash(s));
+                if (listOfHashes.Contains(hash))
+                    matchingStrings.Add(s);
+            }
+            return matchingStrings;
         }
     }
 }
