43951b019bebdbf44b1d66d74806fc63253d6b6b
Carmine Giugliano <neos7@live.it>
2020-06-15 15:46:04 +0200

    update some pgDictionary structs (TODO: add pgDictionary)

--- RageLib.GTA5/Resources/PC/Particles/EmitterRuleDictionary.cs
+++ RageLib.GTA5/Resources/PC/Particles/EmitterRuleDictionary.cs
@@ -21,71 +21,39 @@
 */
 
 using RageLib.Resources.Common;
-using System.Collections.Generic;
+using System;
 
 namespace RageLib.Resources.GTA5.PC.Particles
 {
     // pgBase
     // pgDictionaryBase
     // pgDictionary<ptxEmitterRule>
-    public class EmitterRuleDictionary : ResourceSystemBlock
+    public class EmitterRuleDictionary : FileBase64_GTA5_pc
     {
         public override long Length => 0x40;
 
         // structure data
-        public uint VFT;
-        public uint Unknown_4h; // 0x00000001
-        public uint Unknown_8h; // 0x00000000
-        public uint Unknown_Ch; // 0x00000000
         public uint Unknown_10h; // 0x00000000
         public uint Unknown_14h; // 0x00000000
         public uint Unknown_18h; // 0x00000001
         public uint Unknown_1Ch; // 0x00000000
-        public ulong HashesPointer;
-        public ushort HashesCount1;
-        public ushort HashesCount2;
-        public uint Unknown_2Ch; // 0x00000000
-        public ulong EffectRulesPointer;
-        public ushort EffectRulesCount1;
-        public ushort EffectRulesCount2;
-        public uint Unknown_3Ch; // 0x00000000
-
-        // reference data
-        public ResourceSimpleArray<uint_r> Hashes;
-        public ResourcePointerArray64<EmitterRule> EmitterRules;
+        public ResourceSimpleList64<uint_r> Hashes;
+        public ResourcePointerList64<EmitterRule> EmitterRules;
 
         /// <summary>
         /// Reads the data-block from a stream.
         /// </summary>
         public override void Read(ResourceDataReader reader, params object[] parameters)
         {
+            base.Read(reader, parameters);
+
             // read structure data
-            this.VFT = reader.ReadUInt32();
-            this.Unknown_4h = reader.ReadUInt32();
-            this.Unknown_8h = reader.ReadUInt32();
-            this.Unknown_Ch = reader.ReadUInt32();
             this.Unknown_10h = reader.ReadUInt32();
             this.Unknown_14h = reader.ReadUInt32();
             this.Unknown_18h = reader.ReadUInt32();
             this.Unknown_1Ch = reader.ReadUInt32();
-            this.HashesPointer = reader.ReadUInt64();
-            this.HashesCount1 = reader.ReadUInt16();
-            this.HashesCount2 = reader.ReadUInt16();
-            this.Unknown_2Ch = reader.ReadUInt32();
-            this.EffectRulesPointer = reader.ReadUInt64();
-            this.EffectRulesCount1 = reader.ReadUInt16();
-            this.EffectRulesCount2 = reader.ReadUInt16();
-            this.Unknown_3Ch = reader.ReadUInt32();
-
-            // read reference data
-            this.Hashes = reader.ReadBlockAt<ResourceSimpleArray<uint_r>>(
-                this.HashesPointer, // offset
-                this.HashesCount1
-            );
-            this.EmitterRules = reader.ReadBlockAt<ResourcePointerArray64<EmitterRule>>(
-                this.EffectRulesPointer, // offset
-                this.EffectRulesCount1
-            );
+            this.Hashes = reader.ReadBlock<ResourceSimpleList64<uint_r>>();
+            this.EmitterRules = reader.ReadBlock<ResourcePointerList64<EmitterRule>>();
         }
 
         /// <summary>
@@ -93,40 +61,23 @@ namespace RageLib.Resources.GTA5.PC.Particles
         /// </summary>
         public override void Write(ResourceDataWriter writer, params object[] parameters)
         {
-            // update structure data
-            this.HashesPointer = (ulong)(this.Hashes != null ? this.Hashes.Position : 0);
-            //this.HashesCount1 = (ushort)(this.Hashes != null ? this.Hashes.Count : 0);
-            this.EffectRulesPointer = (ulong)(this.EmitterRules != null ? this.EmitterRules.Position : 0);
-            //this.EffectRulesCount1 = (ushort)(this.EffectRules != null ? this.EffectRules.Count : 0);
+            base.Write(writer, parameters);
 
             // write structure data
-            writer.Write(this.VFT);
-            writer.Write(this.Unknown_4h);
-            writer.Write(this.Unknown_8h);
-            writer.Write(this.Unknown_Ch);
             writer.Write(this.Unknown_10h);
             writer.Write(this.Unknown_14h);
             writer.Write(this.Unknown_18h);
             writer.Write(this.Unknown_1Ch);
-            writer.Write(this.HashesPointer);
-            writer.Write(this.HashesCount1);
-            writer.Write(this.HashesCount2);
-            writer.Write(this.Unknown_2Ch);
-            writer.Write(this.EffectRulesPointer);
-            writer.Write(this.EffectRulesCount1);
-            writer.Write(this.EffectRulesCount2);
-            writer.Write(this.Unknown_3Ch);
+            writer.WriteBlock(this.Hashes);
+            writer.WriteBlock(this.EmitterRules);
         }
 
-        /// <summary>
-        /// Returns a list of data blocks which are referenced by this block.
-        /// </summary>
-        public override IResourceBlock[] GetReferences()
+        public override Tuple<long, IResourceBlock>[] GetParts()
         {
-            var list = new List<IResourceBlock>();
-            if (Hashes != null) list.Add(Hashes);
-            if (EmitterRules != null) list.Add(EmitterRules);
-            return list.ToArray();
+            return new Tuple<long, IResourceBlock>[] {
+                new Tuple<long, IResourceBlock>(0x20, Hashes),
+                new Tuple<long, IResourceBlock>(0x30, EmitterRules)
+            };
         }
     }
 }
--- RageLib.GTA5/Resources/PC/Particles/ParticleRuleDictionary.cs
+++ RageLib.GTA5/Resources/PC/Particles/ParticleRuleDictionary.cs
@@ -28,20 +28,16 @@ namespace RageLib.Resources.GTA5.PC.Particles
     // pgBase
     // pgDictionaryBase
     // pgDictionary<ptxParticleRule>
-    public class ParticleRuleDictionary : ResourceSystemBlock
+    public class ParticleRuleDictionary : FileBase64_GTA5_pc
     {
         public override long Length => 0x40;
 
         // structure data
-        public uint VFT;
-        public uint Unknown_4h; // 0x00000001
-        public uint Unknown_8h; // 0x00000000
-        public uint Unknown_Ch; // 0x00000000
         public uint Unknown_10h; // 0x00000000
         public uint Unknown_14h; // 0x00000000
         public uint Unknown_18h; // 0x00000001
         public uint Unknown_1Ch; // 0x00000000
-        public ResourceSimpleList64<uint_r> ParticleRuleNameHashes;
+        public ResourceSimpleList64<uint_r> Hashes;
         public ResourcePointerList64<ParticleRule> ParticleRules;
 
         /// <summary>
@@ -49,16 +45,14 @@ namespace RageLib.Resources.GTA5.PC.Particles
         /// </summary>
         public override void Read(ResourceDataReader reader, params object[] parameters)
         {
+            base.Read(reader, parameters);
+
             // read structure data
-            this.VFT = reader.ReadUInt32();
-            this.Unknown_4h = reader.ReadUInt32();
-            this.Unknown_8h = reader.ReadUInt32();
-            this.Unknown_Ch = reader.ReadUInt32();
             this.Unknown_10h = reader.ReadUInt32();
             this.Unknown_14h = reader.ReadUInt32();
             this.Unknown_18h = reader.ReadUInt32();
             this.Unknown_1Ch = reader.ReadUInt32();
-            this.ParticleRuleNameHashes = reader.ReadBlock<ResourceSimpleList64<uint_r>>();
+            this.Hashes = reader.ReadBlock<ResourceSimpleList64<uint_r>>();
             this.ParticleRules = reader.ReadBlock<ResourcePointerList64<ParticleRule>>();
         }
 
@@ -67,23 +61,21 @@ namespace RageLib.Resources.GTA5.PC.Particles
         /// </summary>
         public override void Write(ResourceDataWriter writer, params object[] parameters)
         {
+            base.Write(writer, parameters);
+
             // write structure data
-            writer.Write(this.VFT);
-            writer.Write(this.Unknown_4h);
-            writer.Write(this.Unknown_8h);
-            writer.Write(this.Unknown_Ch);
             writer.Write(this.Unknown_10h);
             writer.Write(this.Unknown_14h);
             writer.Write(this.Unknown_18h);
             writer.Write(this.Unknown_1Ch);
-            writer.WriteBlock(this.ParticleRuleNameHashes);
+            writer.WriteBlock(this.Hashes);
             writer.WriteBlock(this.ParticleRules);
         }
 
         public override Tuple<long, IResourceBlock>[] GetParts()
         {
             return new Tuple<long, IResourceBlock>[] {
-                new Tuple<long, IResourceBlock>(0x20, ParticleRuleNameHashes),
+                new Tuple<long, IResourceBlock>(0x20, Hashes),
                 new Tuple<long, IResourceBlock>(0x30, ParticleRules)
             };
         }
