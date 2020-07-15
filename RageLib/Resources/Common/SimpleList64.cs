bd9e61ac539b15a108c610274ca250c54467e3d3
Carmine Giugliano <neos7@live.it>
2020-07-15 20:01:44 +0200

    Added SimpleList64

---
+++ RageLib/Resources/Common/SimpleList64.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace RageLib.Resources.Common
+{
+    public class SimpleList64<T> : ResourceSystemBlock where T : unmanaged
+    {
+        public override long BlockLength
+        {
+            get { return 16; }
+        }
+
+        // structure data
+        public ulong EntriesPointer;
+        public ushort EntriesCount;
+        public ushort EntriesCapacity;
+
+        // reference data
+        public SimpleArray<T> Entries;
+
+        /// <summary>
+        /// Reads the data-block from a stream.
+        /// </summary>
+        public override void Read(ResourceDataReader reader, params object[] parameters)
+        {
+            // read structure data
+            this.EntriesPointer = reader.ReadUInt64();
+            this.EntriesCount = reader.ReadUInt16();
+            this.EntriesCapacity = reader.ReadUInt16();
+            reader.Position += 4;
+
+            // read reference data
+            this.Entries = reader.ReadBlockAt<SimpleArray<T>>(
+                this.EntriesPointer, // offset
+                this.EntriesCount
+            );
+        }
+
+        /// <summary>
+        /// Writes the data-block to a stream.
+        /// </summary>
+        public override void Write(ResourceDataWriter writer, params object[] parameters)
+        {
+            // update structure data
+            this.EntriesPointer = (ulong)(this.Entries != null ? this.Entries.BlockPosition : 0);
+            this.EntriesCount = (ushort)(this.Entries != null ? this.Entries.Count : 0);
+            this.EntriesCapacity = (ushort)(this.Entries != null ? this.Entries.Count : 0);
+
+            // write structure data
+            writer.Write(this.EntriesPointer);
+            writer.Write(this.EntriesCount);
+            writer.Write(this.EntriesCapacity);
+            writer.Write((uint)0x00000000);
+        }
+
+        /// <summary>
+        /// Returns a list of data blocks which are referenced by this block.
+        /// </summary>
+        public override IResourceBlock[] GetReferences()
+        {
+            var list = new List<IResourceBlock>();
+            if (Entries != null) list.Add(Entries);
+            return list.ToArray();
+        }
+    }
+}
