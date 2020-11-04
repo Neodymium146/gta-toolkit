b4fdce5cd95351a73b7081d095490be446b0668c
Carmine Giugliano <neos7@live.it>
2020-11-04 11:14:47 +0100

    RageLib.GTA5: add more resource file wrappers

---
+++ RageLib.GTA5/ResourceWrappers/PC/Clips/ClipDictionaryFileWrapper_GTA5_pc.cs
@@ -0,0 +1,43 @@
+﻿using RageLib.Resources.GTA5;
+using RageLib.Resources.GTA5.PC.Clips;
+using System.IO;
+
+namespace RageLib.GTA5.ResourceWrappers.PC.Clips
+{
+    public class ClipDictionaryFileWrapper_GTA5_pc
+    {
+        private ClipDictionary clipDictionary;
+
+        public void Load(Stream stream)
+        {
+            var resource = new ResourceFile_GTA5_pc<ClipDictionary>();
+            resource.Load(stream);
+
+            clipDictionary = resource.ResourceData;
+        }
+
+        public void Load(string fileName)
+        {
+            var resource = new ResourceFile_GTA5_pc<ClipDictionary>();
+            resource.Load(fileName);
+
+            clipDictionary = resource.ResourceData;
+        }
+
+        public void Save(Stream stream)
+        {
+            var resource = new ResourceFile_GTA5_pc<ClipDictionary>();
+            resource.ResourceData = clipDictionary;
+            resource.Version = 46;
+            resource.Save(stream);
+        }
+
+        public void Save(string fileName)
+        {
+            var resource = new ResourceFile_GTA5_pc<ClipDictionary>();
+            resource.ResourceData = clipDictionary;
+            resource.Version = 46;
+            resource.Save(fileName);
+        }
+    }
+}
---
+++ RageLib.GTA5/ResourceWrappers/PC/Clothes/ClothDictionaryFileWrapper_GTA5_pc.cs
@@ -0,0 +1,44 @@
+﻿using RageLib.Resources.Common;
+using RageLib.Resources.GTA5;
+using RageLib.Resources.GTA5.PC.Clothes;
+using System.IO;
+
+namespace RageLib.GTA5.ResourceWrappers.PC.Clothes
+{
+    public class ClothDictionaryFileWrapper_GTA5_pc
+    {
+        private PgDictionary64<CharacterCloth> clothDictionary;
+
+        public void Load(Stream stream)
+        {
+            var resource = new ResourceFile_GTA5_pc<PgDictionary64<CharacterCloth>>();
+            resource.Load(stream);
+
+            clothDictionary = resource.ResourceData;
+        }
+
+        public void Load(string fileName)
+        {
+            var resource = new ResourceFile_GTA5_pc<PgDictionary64<CharacterCloth>>();
+            resource.Load(fileName);
+
+            clothDictionary = resource.ResourceData;
+        }
+
+        public void Save(Stream stream)
+        {
+            var resource = new ResourceFile_GTA5_pc<PgDictionary64<CharacterCloth>>();
+            resource.ResourceData = clothDictionary;
+            resource.Version = 8;
+            resource.Save(stream);
+        }
+
+        public void Save(string fileName)
+        {
+            var resource = new ResourceFile_GTA5_pc<PgDictionary64<CharacterCloth>>();
+            resource.ResourceData = clothDictionary;
+            resource.Version = 8;
+            resource.Save(fileName);
+        }
+    }
+}
