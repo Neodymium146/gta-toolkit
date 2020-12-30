5ea92b9d29fc32003a32f3f8984495bb96c20f77
carmineos <neos7@live.it>
2020-12-30 17:54:38 +0100

    RageLib.GTA5: update JointType structs

--- RageLib.GTA5/Resources/PC/Fragments/Joint3DofType.cs
+++ RageLib.GTA5/Resources/PC/Fragments/Joint3DofType.cs
@@ -20,6 +20,8 @@
     THE SOFTWARE.
 */
 
+using System.Numerics;
+
 namespace RageLib.Resources.GTA5.PC.Fragments
 {
     // phJoint3DofType
@@ -28,58 +30,19 @@ namespace RageLib.Resources.GTA5.PC.Fragments
         public override long BlockLength => 0xF0;
 
         // structure data
-        public uint Unknown_20h;
-        public uint Unknown_24h;
-        public uint Unknown_28h;
-        public uint Unknown_2Ch;
-        public uint Unknown_30h;
-        public uint Unknown_34h;
-        public uint Unknown_38h;
-        public uint Unknown_3Ch;
-        public uint Unknown_40h;
-        public uint Unknown_44h;
-        public uint Unknown_48h;
-        public uint Unknown_4Ch;
-        public uint Unknown_50h;
-        public uint Unknown_54h;
-        public uint Unknown_58h;
-        public uint Unknown_5Ch; // 0x7FC00001
-        public uint Unknown_60h;
-        public uint Unknown_64h;
-        public uint Unknown_68h;
-        public uint Unknown_6Ch;
-        public uint Unknown_70h;
-        public uint Unknown_74h;
-        public uint Unknown_78h;
-        public uint Unknown_7Ch;
-        public uint Unknown_80h;
-        public uint Unknown_84h;
-        public uint Unknown_88h;
-        public uint Unknown_8Ch;
-        public uint Unknown_90h;
-        public uint Unknown_94h;
-        public uint Unknown_98h;
-        public uint Unknown_9Ch; // 0x7FC00001
-        public uint Unknown_A0h;
-        public uint Unknown_A4h;
-        public uint Unknown_A8h;
-        public uint Unknown_ACh; // 0x00000000
-        public uint Unknown_B0h; // 0x00000000
-        public uint Unknown_B4h; // 0x00000000
-        public uint Unknown_B8h; // 0x00000000
-        public uint Unknown_BCh; // 0x00000000
-        public uint Unknown_C0h; // 0x4CBEBC20
-        public uint Unknown_C4h; // 0x4CBEBC20
-        public uint Unknown_C8h; // 0x4CBEBC20
-        public uint Unknown_CCh; // 0x4CBEBC20
-        public uint Unknown_D0h; // 0xCCBEBC20
-        public uint Unknown_D4h; // 0xCCBEBC20
-        public uint Unknown_D8h; // 0xCCBEBC20
-        public uint Unknown_DCh; // 0xCCBEBC20
-        public uint Unknown_E0h; // 0x00000000
-        public uint Unknown_E4h; // 0x00000000
-        public uint Unknown_E8h; // 0x00000000
-        public uint Unknown_ECh; // 0x00000000
+        public Vector4 Unknown_20h;
+        public Vector4 Unknown_30h;
+        public Vector4 Unknown_40h;
+        public Vector4 Unknown_50h;
+        public Vector4 Unknown_60h;
+        public Vector4 Unknown_70h;
+        public Vector4 Unknown_80h;
+        public Vector4 Unknown_90h;
+        public Vector4 Unknown_A0h;
+        public Vector4 Unknown_B0h; // 0x00000000
+        public Vector4 Unknown_C0h; // 0x4CBEBC20
+        public Vector4 Unknown_D0h; // 0xCCBEBC20
+        public Vector4 Unknown_E0h; // 0x00000000
 
         /// <summary>
         /// Reads the data-block from a stream.
@@ -89,58 +52,19 @@ namespace RageLib.Resources.GTA5.PC.Fragments
             base.Read(reader, parameters);
 
             // read structure data
-            this.Unknown_20h = reader.ReadUInt32();
-            this.Unknown_24h = reader.ReadUInt32();
-            this.Unknown_28h = reader.ReadUInt32();
-            this.Unknown_2Ch = reader.ReadUInt32();
-            this.Unknown_30h = reader.ReadUInt32();
-            this.Unknown_34h = reader.ReadUInt32();
-            this.Unknown_38h = reader.ReadUInt32();
-            this.Unknown_3Ch = reader.ReadUInt32();
-            this.Unknown_40h = reader.ReadUInt32();
-            this.Unknown_44h = reader.ReadUInt32();
-            this.Unknown_48h = reader.ReadUInt32();
-            this.Unknown_4Ch = reader.ReadUInt32();
-            this.Unknown_50h = reader.ReadUInt32();
-            this.Unknown_54h = reader.ReadUInt32();
-            this.Unknown_58h = reader.ReadUInt32();
-            this.Unknown_5Ch = reader.ReadUInt32();
-            this.Unknown_60h = reader.ReadUInt32();
-            this.Unknown_64h = reader.ReadUInt32();
-            this.Unknown_68h = reader.ReadUInt32();
-            this.Unknown_6Ch = reader.ReadUInt32();
-            this.Unknown_70h = reader.ReadUInt32();
-            this.Unknown_74h = reader.ReadUInt32();
-            this.Unknown_78h = reader.ReadUInt32();
-            this.Unknown_7Ch = reader.ReadUInt32();
-            this.Unknown_80h = reader.ReadUInt32();
-            this.Unknown_84h = reader.ReadUInt32();
-            this.Unknown_88h = reader.ReadUInt32();
-            this.Unknown_8Ch = reader.ReadUInt32();
-            this.Unknown_90h = reader.ReadUInt32();
-            this.Unknown_94h = reader.ReadUInt32();
-            this.Unknown_98h = reader.ReadUInt32();
-            this.Unknown_9Ch = reader.ReadUInt32();
-            this.Unknown_A0h = reader.ReadUInt32();
-            this.Unknown_A4h = reader.ReadUInt32();
-            this.Unknown_A8h = reader.ReadUInt32();
-            this.Unknown_ACh = reader.ReadUInt32();
-            this.Unknown_B0h = reader.ReadUInt32();
-            this.Unknown_B4h = reader.ReadUInt32();
-            this.Unknown_B8h = reader.ReadUInt32();
-            this.Unknown_BCh = reader.ReadUInt32();
-            this.Unknown_C0h = reader.ReadUInt32();
-            this.Unknown_C4h = reader.ReadUInt32();
-            this.Unknown_C8h = reader.ReadUInt32();
-            this.Unknown_CCh = reader.ReadUInt32();
-            this.Unknown_D0h = reader.ReadUInt32();
-            this.Unknown_D4h = reader.ReadUInt32();
-            this.Unknown_D8h = reader.ReadUInt32();
-            this.Unknown_DCh = reader.ReadUInt32();
-            this.Unknown_E0h = reader.ReadUInt32();
-            this.Unknown_E4h = reader.ReadUInt32();
-            this.Unknown_E8h = reader.ReadUInt32();
-            this.Unknown_ECh = reader.ReadUInt32();
+            this.Unknown_20h = reader.ReadVector4();
+            this.Unknown_30h = reader.ReadVector4();
+            this.Unknown_40h = reader.ReadVector4();
+            this.Unknown_50h = reader.ReadVector4();
+            this.Unknown_60h = reader.ReadVector4();
+            this.Unknown_70h = reader.ReadVector4();
+            this.Unknown_80h = reader.ReadVector4();
+            this.Unknown_90h = reader.ReadVector4();
+            this.Unknown_A0h = reader.ReadVector4();
+            this.Unknown_B0h = reader.ReadVector4();
+            this.Unknown_C0h = reader.ReadVector4();
+            this.Unknown_D0h = reader.ReadVector4();
+            this.Unknown_E0h = reader.ReadVector4();
         }
 
         /// <summary>
@@ -152,57 +76,18 @@ namespace RageLib.Resources.GTA5.PC.Fragments
 
             // write structure data
             writer.Write(this.Unknown_20h);
-            writer.Write(this.Unknown_24h);
-            writer.Write(this.Unknown_28h);
-            writer.Write(this.Unknown_2Ch);
             writer.Write(this.Unknown_30h);
-            writer.Write(this.Unknown_34h);
-            writer.Write(this.Unknown_38h);
-            writer.Write(this.Unknown_3Ch);
             writer.Write(this.Unknown_40h);
-            writer.Write(this.Unknown_44h);
-            writer.Write(this.Unknown_48h);
-            writer.Write(this.Unknown_4Ch);
             writer.Write(this.Unknown_50h);
-            writer.Write(this.Unknown_54h);
-            writer.Write(this.Unknown_58h);
-            writer.Write(this.Unknown_5Ch);
             writer.Write(this.Unknown_60h);
-            writer.Write(this.Unknown_64h);
-            writer.Write(this.Unknown_68h);
-            writer.Write(this.Unknown_6Ch);
             writer.Write(this.Unknown_70h);
-            writer.Write(this.Unknown_74h);
-            writer.Write(this.Unknown_78h);
-            writer.Write(this.Unknown_7Ch);
             writer.Write(this.Unknown_80h);
-            writer.Write(this.Unknown_84h);
-            writer.Write(this.Unknown_88h);
-            writer.Write(this.Unknown_8Ch);
             writer.Write(this.Unknown_90h);
-            writer.Write(this.Unknown_94h);
-            writer.Write(this.Unknown_98h);
-            writer.Write(this.Unknown_9Ch);
             writer.Write(this.Unknown_A0h);
-            writer.Write(this.Unknown_A4h);
-            writer.Write(this.Unknown_A8h);
-            writer.Write(this.Unknown_ACh);
             writer.Write(this.Unknown_B0h);
-            writer.Write(this.Unknown_B4h);
-            writer.Write(this.Unknown_B8h);
-            writer.Write(this.Unknown_BCh);
             writer.Write(this.Unknown_C0h);
-            writer.Write(this.Unknown_C4h);
-            writer.Write(this.Unknown_C8h);
-            writer.Write(this.Unknown_CCh);
             writer.Write(this.Unknown_D0h);
-            writer.Write(this.Unknown_D4h);
-            writer.Write(this.Unknown_D8h);
-            writer.Write(this.Unknown_DCh);
             writer.Write(this.Unknown_E0h);
-            writer.Write(this.Unknown_E4h);
-            writer.Write(this.Unknown_E8h);
-            writer.Write(this.Unknown_ECh);
         }
     }
 }
