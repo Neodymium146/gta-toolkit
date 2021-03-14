2f79f196689fadd6ef2454a873529be7c0a10c91
carmineos <neos7@live.it>
2021-03-14 21:26:09 +0100

    Replace RAGE_Matrix3x4 with Matrix3x4 struct

---
+++ RageLib/Resources/Common/Matrix3x4.cs
@@ -0,0 +1,22 @@
+﻿using RageLib.Data;
+using System.Numerics;
+
+namespace RageLib.Resources.Common
+{
+    public struct Matrix3x4 : IResourceStruct<Matrix3x4>
+    {
+        public Vector4 Row1;
+        public Vector4 Row2;
+        public Vector4 Row3;
+
+        public Matrix3x4 ReverseEndianness()
+        {
+            return new Matrix3x4()
+            {
+                Row1 = EndiannessExtensions.ReverseEndianness(Row1),
+                Row2 = EndiannessExtensions.ReverseEndianness(Row2),
+                Row3 = EndiannessExtensions.ReverseEndianness(Row3)
+            };
+        }
+    }
+}
